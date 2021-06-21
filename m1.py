6610f12850611c1969adee07bd3de69c7d5bde38
Hossam <hossamfathi050@outlook.com>
2021-06-21 17:12:26 +0300

    Add Python file about Calender

---
+++ m1.py
@@ -0,0 +1,13 @@
+import calendar
+cal=calendar.month(2021,6)
+print (cal)
+
+print("===================")
+
+print (calendar.calendar(2021)) 
+
+print("===================")
+ 
+x = calendar.weekday(2021, 6, 21)
+print("Weekday number for the given date:", x)
+print()
